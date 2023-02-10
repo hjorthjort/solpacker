2428974292cb802b234900017686380c7e4905d8
hjort <8545447+hjorthjort@users.noreply.github.com>
2023-02-10 18:15:40 +0100

    example contract

---
+++ Example.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.8.0;
+
+contract Example {
+    uint256 v1;
+    uint8   v2;
+    uint256 v3;
+    uint32  v4;
+    uint16  v5;
+    uint256 v7;
+    uint128 v8;
+    address v9;
+    address v10;
+    uint64[] v11;
+    mapping (address => bytes) v12;
+    bool v13;
+    S v14;
+    uint8 v15;
+
+    struct S {
+        bool s1;
+        uint256 s2;
+        address s3;
+    }
+}
